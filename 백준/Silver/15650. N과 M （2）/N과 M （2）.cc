33bbf5cbd52f8a2f01aa055a413c5231e8ea1c91
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2025-01-08 01:16:50 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int st = 1; // 시작점
+	if (k != 0) st = arr[k - 1] + 1; // 선택된 수들이 항상 오름차순, 1회차 제외
+
+	for (int i = st; i <= n; i++) {
+		arr[k] = i;
+		func(k + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	func(0);
+}
