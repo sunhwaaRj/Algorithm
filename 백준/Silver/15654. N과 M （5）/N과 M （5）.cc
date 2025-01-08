47828ba38678a7d36267a08c73fdc9896a5906a3
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2025-01-08 10:57:44 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int asc[10]; // 입력받은 수가 오름차순 될 자리
+bool isused[10001];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	// 수를 입력받으므로 i는 asc 배열의 주소
+	for (int i = 0; i < n; i++) {
+		if (!isused[asc[i]]) {
+			arr[k] = asc[i];
+			isused[asc[i]] = 1;
+			func(k + 1);
+			isused[asc[i]] = 0;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> asc[i];
+
+	sort(asc, asc + n); // 배열 정렬
+
+	func(0);
+}
