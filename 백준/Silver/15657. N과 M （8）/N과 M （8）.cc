c5a682f0fa69b7da48531f1e1d59b2fb3020ec60
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2025-01-08 12:16:50 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int asc[10];
+
+// 중복 있는 조합(st)
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << asc[arr[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int st = 0;
+	if (k != 0) st = arr[k - 1];
+	for (int i = st; i < n; i++) {
+		arr[k] = i;
+		func(k + 1);
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> asc[i];
+
+	sort(asc, asc + n);
+	func(0);
+}
