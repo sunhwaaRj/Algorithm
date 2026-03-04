325b87a28adb3057d75f57bfa608132bcce9c81e
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-03-05 02:39:53 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,12 +1,6 @@
 function solution(a, b) {
-    var answer = 0;
     var v1 = String(a);
     var v2 = String(b);
     
-    if(Number(v1 + v2) < Number(v2 + v1))
-        answer = Number(v2 + v1);
-    else
-        answer = Number(v1 + v2)
-    
-    return answer;
+    return Math.max(Number(v1+v2), Number(v2+v1))
 }
