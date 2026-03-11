36200bf0329d8b58be6bfeb860bc9b61b6af9236
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-03-12 02:09:17 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.17 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,15 +1,12 @@
 function solution(arr, queries) {
-    var answer = [];
-    const MIN = 1000000;
-    for(const query of queries){
-	    var min = MIN;
-	    for(var i=query[0];i<=query[1];i++){
-		    if(arr[i] > query[2] & arr[i] < min){
-			    min = arr[i]
-		    }
+    return queries.map(([s, e, k]) => {
+	    let result = -1
+	    for(var i=s;i<=e;i++){
+		    let v = arr[i];
+		    if(v <= k) continue; //값이 k 보다 작거나 같으면 의미 없음
+		    // result 가 -1 이면 v 값, 아니면 result 와 현재 값 비교하여 작은것
+		    result = result === -1 ? v : Math.min(result, v);		    
 	    }
-	    if(min === MIN) answer.push(-1);
-	    else answer.push(min);
-    }
-    return answer;
+	    return result ;
+    })
 }
