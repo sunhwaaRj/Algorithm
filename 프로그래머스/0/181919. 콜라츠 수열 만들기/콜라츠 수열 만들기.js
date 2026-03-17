0e2ea66bdcb7776cba351195dcae433bf36e8880
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-03-17 20:40:02 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  var answer = [];
+  answer.push(n);
+
+  while (n !== 1) {
+    if (n % 2 == 0) n = n / 2;
+    else n = 3 * n + 1;
+    answer.push(n);
+  }
+  return answer;
+}
