556ceff7123c5c6d71de81d46ed1498f50e92dd3
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-03-30 22:24:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    var answer = 0;
+    if (num == 1) return 0;
+    while(num !== 1){
+        if(num % 2 == 0) num /= 2;
+        else num = num * 3 + 1;
+        answer ++;
+    }
+    
+    return answer > 500 ? -1 : answer;
+}
