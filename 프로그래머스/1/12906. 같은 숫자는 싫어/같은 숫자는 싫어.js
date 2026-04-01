e0f713282e5c14e1d0157bd9e96d01512bff57a5
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-04-02 00:29:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.78 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,4 @@
 function solution(arr)
 {
-    var answer = [];
-    // arr[i] 과 [i+1] 이 다를 때만 answer 에 추가
-    for(var i=0 ; i<arr.length;i++){
-        if(arr[i] !== arr[i+1]) answer.push(arr[i]);
-    }
-    
-    return answer;
+    return arr.filter((value, index) => value !== arr[index+1]);
 }
