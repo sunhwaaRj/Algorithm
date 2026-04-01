68670c97537aed8d58a27d57295486429fcf9e78
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-04-02 02:21:07 +0900

    [level 1] Title: 최소직사각형, Time: 25.56 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,20 +1,11 @@
 function solution(sizes) {
-    var answer = 0;
-    var min = [];
-    var max = [];
-    // [0][0] / [0][1] 중에 작은거
-    // [1][0] / [1][1] 중에 작은거
-    // [2][0] / [2][1] 중에 작은거
-    //...
-    // [sizes.length - 1][0] / [sizes.length - 1][1] 중에 작은거
-    // 작은거 끼리 비교해서 max 구하기
-    // 큰거끼리 비교해서 max 구하기
-    for(var i=0;i<sizes.length;i++){
-        min.push(Math.min(sizes[i][0], sizes[i][1]));
-        max.push(Math.max(sizes[i][0], sizes[i][1]))
-    }
-    min.sort((a, b) => b-a);
-    max.sort((a, b) => b-a);
+    var answer = [0, 0];
     
-    return min[0]*max[0];
+    const sort = sizes.map(([w, h]) => w < h ? [h, w]:[w, h]) // 작은 숫자가 나중
+    sort.forEach(([w, h]) => { // 구조분해할당 사용
+        if (w > answer[0]) answer[0] = w;
+        if (h > answer[1]) answer[1] = h;
+    })
+
+    return answer[0]*answer[1];
 }
