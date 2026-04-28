e94a95aa58b5f29a1f9a7bd6528c12b5f831f2ba
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-04-28 16:54:49 +0900

    [level 1] Title: 모의고사, Time: 2.86 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,21 +1,17 @@
 function solution(answers) {
     var answer = [];
-    var [one, two, three] = [0, 0, 0];
     var oo = [1, 2, 3, 4, 5];
     var tw = [2, 1, 2, 3, 2, 4, 2, 5];
     var th = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    for(var i in answers){
-        if(answers[i] == oo[i%5]) one++;
-        if(answers[i] == tw[i%8]) two++;
-        if(answers[i] == th[i%10]) three++;
-    }
+    var one = answers.filter((a, i) => a === oo[i%5]).length;
+    var two = answers.filter((a, i) => a === tw[i%8]).length;
+    var three = answers.filter((a, i) => a === th[i%10]).length;
     
     var max = Math.max(one, two, three);
-    var arr = [one, two, three];
-    for(var j in arr){
-        if(arr[j] == max) answer.push(+j+1);
-    }
+    if(one === max) answer.push(1);
+    if(two === max) answer.push(2);
+    if(three === max) answer.push(3);
     
     return answer;
 }
