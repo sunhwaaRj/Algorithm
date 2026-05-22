2fdf3109815a620b02d3edc399eee4cc7adbd09c
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2026-05-23 01:21:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,16 +1,8 @@
 function solution(lottos, win_nums) {
-    var [n, m] = [0, 0];
-    var [min, max] = [0, 0];
-    for(var i of lottos){
-        if(win_nums.includes(i)) n++;
-        if(i === 0) m++;
-    }
+  const rank = [6, 6, 5, 4, 3, 2, 1];
     
-    min = 7-m-n;
-    max = 7-n;
-    
-    if(min >= 7) min = 6;
-    if(max >= 7) max = 6;
-        
-    return [min, max];
+  const min = lottos.filter((x) => win_nums.includes(x)).length;
+  const max = lottos.filter((x) => x === 0).length + min;
+
+  return [rank[max], rank[min]];
 }
